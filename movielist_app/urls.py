b5fa3b1bd94f2130fb9788be44293c9c73d09412
adityash97 <adityash212@gmail.com>
2023-07-13 23:15:23 +0530

    https://github.com/adityash97/drf_practice/issues/1 : DB deleted2

--- movielist_app/urls.py
+++ movielist_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import MovieAPIView,GenreAPIView,DirectorAPIView,MovieDetailsAPIView
+from .views import MovieAPIView,GenreAPIView,DirectorAPIView,MovieDetailsAPIView,GenreDetailAPIView,DirectorDetailAPIView
 
 urlpatterns = [
     path('', MovieAPIView.as_view(),name='movie_api'),
     path('<int:pk>/', MovieDetailsAPIView.as_view(),name='movie_details_api'),
     path('genre/',GenreAPIView.as_view(),name="genre_api"),
-    path('director/',DirectorAPIView.as_view(),name="director_api")
+    path('genre/<int:pk>/',GenreDetailAPIView.as_view(),name="genre_detail_api"),
+    path('director/',DirectorAPIView.as_view(),name="director_api"),
+    path('director/<int:pk>/',DirectorDetailAPIView.as_view(),name="director_detail_api")
 ]
--- movielist_app/views.py
+++ movielist_app/views.py
@@ -60,16 +60,77 @@ class MovieDetailsAPIView(APIView):
 
 
 class GenreAPIView(APIView):
+    # Post is not allowed as we have predefined values here. 
+    #TODO : add others field
     
     def get(self,request):
         genre = Genre.objects.all()
         serializer = GenreSerializer(genre,many = True)
         return Response(serializer.data)
-    def post(self,request):
-        import pdb;pdb.set_trace()
+class GenreDetailAPIView(APIView):
+    def get_object(self,pk):
+        try:
+            return Genre.objects.get(pk=pk)
+        except Genre.DoesNotExist:
+            raise Http404
+    def get(self,request,pk):
+        genre = self.get_object(pk)
+        serilaizer = GenreSerializer(genre)
+        return Response(serilaizer.data)
+        
+    def put(self,request,pk):
+        genre = self.get_object(pk)
+        serializer = GenreSerializer(genre,data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+    
+    
+    def delete(self,request,pk):
+        genre = self.get_object(pk)
+        genre.delete()
+        return Response({'msg':"Sucessfully Deleted"})
+        
+        
+    
+    
     
 class DirectorAPIView(APIView):
     def get(self,request):
         director = Director.objects.all()
         serializer = DirectorSerializer(director,many=True)
         return Response(serializer.data)
+    
+    def post(self,request):
+        serializer = DirectorSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
+class DirectorDetailAPIView(APIView):
+    def get_object(self,pk):
+        try:
+            return Director.objects.get(pk=pk)
+        except Director.DoesNotExist:
+            raise Http404
+    
+    def get(self,request,pk):
+        director = self.get_object(pk)
+        serializer = DirectorSerializer(director)
+        return Response(serializer.data)
+        
+    def put(self,request,pk):
+        director = self.get_object(pk)
+        serializer = DirectorSerializer(director,data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+    
+    def delete(self,request,pk):
+        director = self.get_object(pk)
+        director.delete()
+        return Response({"msg":'Sucessfully Deleted'})
+        

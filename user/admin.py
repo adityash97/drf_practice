bbfb86c46c7f38804eef435c4073c66f9067a613
adityash97 <adityash212@gmail.com>
2023-07-13 23:21:57 +0530

    db updated

--- user/admin.py
+++ user/admin.py
@@ -1,12 +1,5 @@
 from django.contrib import admin
 from .models import User
-<<<<<<< HEAD
-from .models import Token
-# Register your models here.
-
-admin.site.register(User)
-admin.site.register(Token)
-=======
 from .models import Token 
 from .models import MovieRatingDetail
 # Register your models here.
@@ -14,4 +7,3 @@ from .models import MovieRatingDetail
 admin.site.register(User)
 admin.site.register(Token)
 admin.site.register(MovieRatingDetail)
->>>>>>> 012a879 (https://github.com/adityash97/drf_practice/issues/1 : CRUD complete)

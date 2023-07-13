a0e99e40d2dfa5929c73060556cace99464a7519
adityash97 <adityash212@gmail.com>
2023-07-13 23:14:24 +0530

    https://github.com/adityash97/drf_practice/issues/1 : DB deleted

--- movielist_app/serializers.py
+++ movielist_app/serializers.py
@@ -5,14 +5,20 @@ from .models import Director
 
 class MovieSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=500)
-    description = serializers.CharField()
-    average_rating = serializers.IntegerField(default=0)
-    total_like = serializers.IntegerField(default=0)
-    total_review = serializers.IntegerField(default=0)
-    released_date = serializers.DateField()
-    duration = serializers.CharField(max_length=100)
-    director = serializers.PrimaryKeyRelatedField(queryset=Director.objects.all())
-    genre = serializers.PrimaryKeyRelatedField(many=True,queryset=Genre.objects.all())
+    description = serializers.CharField(required=False)
+    average_rating = serializers.IntegerField(required=False)
+    total_like = serializers.IntegerField(required=False)
+    total_review = serializers.IntegerField(required=False)
+    released_date = serializers.DateField(required=False)
+    duration = serializers.CharField(max_length=100,required=False)
+    director = serializers.PrimaryKeyRelatedField(queryset=Director.objects.all(),required=False)
+    genre = serializers.PrimaryKeyRelatedField(many=True,queryset=Genre.objects.all(),required=False)
+    
+    def create(self,validated_data):
+        genre_ids =validated_data.pop('genre',[])
+        movie = Movie.objects.create(**validated_data)
+        movie.genre.set(genre_ids)
+        return movie
 
 class GenreSerializer(serializers.Serializer):
     genre = serializers.CharField()
--- movielist_app/views.py
+++ movielist_app/views.py
@@ -7,22 +7,29 @@ from rest_framework.response import Response
 from .serializers import MovieSerializer,GenreSerializer,DirectorSerializer
 from .models import Movie,Genre,Director
 
+from rest_framework.parsers import JSONParser
 class MovieAPIView(APIView):
     def get(self,request):
         movies = Movie.objects.all()
         movieSerializer = MovieSerializer(movies,many=True)
-        
         return Response(movieSerializer.data)
+    def post(self,request):
+        serializer = MovieSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors) 
 
 class GenreAPIView(APIView):
     def get(self,request):
         genre = Genre.objects.all()
         serializer = GenreSerializer(genre,many = True)
         return Response(serializer.data)
+    def post(self,request):
+        import pdb;pdb.set_trace()
     
 class DirectorAPIView(APIView):
     def get(self,request):
         director = Director.objects.all()
         serializer = DirectorSerializer(director,many=True)
-        
         return Response(serializer.data)
